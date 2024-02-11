e7e0dc04ca305031785cd227cdff9d1fac1e8e5f
Maksudur20 <20maksudur00@gmail.com>
2024-02-11 20:37:13 +0600

    v1

--- main.js
+++ main.js
@@ -10,3 +10,20 @@ registerLink.addEventListener('click', () => {
 loginLink.addEventListener('click', () => {
     logregBox.classList.remove('active');
 });
+
+
+
+let formId = document.getElemenyById("login-form");
+
+formId.addEventlistener('submit', (e)=>{
+ let email = document.getElementById("email").value;
+let pass = document.getElementByid("password").value;
+
+  If(email === "admin@gmail.com" && pass === "admin"){
+    console.log("logged in");
+}
+else
+{
+ console.log("Wrong Pass");
+}
+})
